48fb749d77afc92bbc843be7e13b7f19969363e0
MASTERALD <93801083+MASTERALD@users.noreply.github.com>
2022-01-18 16:15:11 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-const mostrar = document.getElementById('menu')
-const menu = document.getElementById('mostrar')
-const reproducir = document.getElementById('reproducir')
-const play = document.getElementById('play')
-reproducir.addEventListener('click', () =>{
-    play.play() 
-}) 
-let cont =1
-mostrar.addEventListener('click', () =>{
-    if(cont == 1){
-        menu.style.right = "1em"
-        menu.style.transition = "all .3s"
-        cont =0;
-    }else{
-        cont =1;
-        menu.style.right = "-11em"
-        menu.style.transition = "all .3s"
-    }
-})
+/* Hides the body scrollbar when the overlay menu is opened */
+
+// Get the body element
+const body = document.body;
+
+// Get the checkbox element
+const hamburgerToggler = document.querySelector(".toggler");
+
+// Set event listener to the hamburger toggler
+// so that we can detect when the overlay is opened and hide the body scrollbar
+hamburgerToggler.addEventListener("change", (e) => {
+  e.target.checked
+    ? (body.style.overflowY = "hidden")
+    : (body.style.overflowY = "scroll");
+});
